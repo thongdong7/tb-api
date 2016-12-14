eb8f0abb51c96496196ae548cb7ac98274a1a15a
Thong Dong <thongdong7@gmail.com>
2016-12-14 07:47:03 +0700

    router: add unit test

--- tests_tb_api/TestRouter.py
+++ tests_tb_api/TestRouter.py
@@ -19,6 +19,14 @@ class TestRouterTestCase(unittest.TestCase):
         self.assertEqual('p2', actual.data)
         self.assertEqual({'client_id': 123}, actual.params)
 
+    def test_02(self):
+        router = PathRouter()
+        router.add_path('get', '/clients/{client_id:int}/{abc}', 'p1')
+
+        actual = router.search('get', '/clients/123/aaa')
+        self.assertTrue(actual.match)
+        self.assertEqual('p1', actual.data)
+        self.assertEqual({'client_id': 123, 'abc': 'aaa'}, actual.params)
 
 if __name__ == '__main__':
     unittest.main()
