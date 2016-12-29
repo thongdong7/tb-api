b27fe744fbc80e1ea9f862ae57f50ff9c27a44e7
Thong Dong <thongdong7@gmail.com>
2016-12-29 17:57:02 +0700

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tb-api',
-    version='1.0.5',
+    version='1.0.6',
     description='Simple Flask API',
     author='Thong Dong',
     author_email='thongdong7@gmail.com',
